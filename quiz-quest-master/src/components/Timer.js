35e6fcac7acea74bbf72a06e30b9367de0d4dfb4
byunn90 <kayhanturk90@Live.com>
2024-06-12 23:56:34 +1000

    timer

--- quiz-quest-master/src/components/Timer.js
+++ quiz-quest-master/src/components/Timer.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function Timer() {
+  useEffect(function () {
+    setInterval();
+  }, []);
+
   return <div className="timer">5:00</div>;
 }
 export default Timer;
